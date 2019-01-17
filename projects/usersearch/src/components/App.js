e85e1c9b2a677d8e50e557ce1edd3e6c8dfdf108
asingjr2 <asingjr2@gmail.com>
2019-01-17 14:32:48 -0500

    updated usersearch project with searchbar complete.  Information includes new passing of function to child compenent.

--- projects/usersearch/src/components/App.js
+++ projects/usersearch/src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-  <div className="ui container" style={{ marginTop: '10px'}}>
-    <h1>
-      APP
-    </h1>
-    <SearchBar  testName="Joe Blow" />
-  </div>)
-};
+// const App = () => {
+//   return (
+//   <div className="ui container" style={{ marginTop: '10px'}}>
+//     <h1>
+//       APP
+//     </h1>
+//     <SearchBar  testName="Joe Blow" />
+//   </div>)
+// };
+
+class App extends React.Component {
+  // Returns submitted search term from submit component {
+    onSearchSubmit(term) {
+      console.log(term);
+    }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px'}}>
+        <h1>APP</h1>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App
 
--- projects/usersearch/src/components/SearchBar.js
+++ projects/usersearch/src/components/SearchBar.js
@@ -1,49 +1,33 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = {term: ''};
+  
+  onFormSubmit = (e) => {
+    e.preventDefault();
 
-  state = {term: '', testName: ''};
-
-  printHello() {
-    console.log("hey friend")
-  }
-
-  componentDidUpdate() {
-    console.log("Updating component")
+    // Using function based down from app.
+    // Passing in state value which will be taken from user from input.
+    this.props.onSubmit(this.state.term);
   }
   
-  // Four input commands listed
-  // First reacts to function
-  // Second reacts to event
-  // Third reacts to event and updates state to fast to notice
-  // Forth reads the update stated value and populates screen based on changes.
+  // Creating controlled element that reacts based on state value.
+  // State 'term' is updated when type, but the component is reloaded so fast its unnoticable.
   render() {
     return (
       <div className="ui segment">
-        <form action="" className="ui form" onClick={this.printHello}>
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
-            <h1>{this.props.testName}</h1>
             <label htmlFor="">Image Search</label>
-            <input type="text" className="field" placeHolder="You are awesome" onChange={this.onInputChange}/>
-            <input type="text" className="field" onChange={e => console.log(e.target.value)}/>
-            <input type="text" className="field" 
-            value={this.state.term} 
-            onChange={e => this.setState({term: e.target.value})} />
-            <input type="text" className="field" value={this.state.term}  />
+            <input type="text" 
+              value={this.state.term} 
+              onChange={e => this.setState({term: e.target.value})} 
+            />
           </div>
         </form>
       </div>
     )
   }
-}
-
-
-// Adding to test if you can add default with only one prop.
-SearchBar.defaultProps = {
-  testName : "Loading your perfect scene..."
 }
 
 export default SearchBar

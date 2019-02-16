1f3b77ec12652710a0805a4a85d00f5748cee66a
asingjr2 <asingjr2@gmail.com>
2019-02-16 15:18:34 -0500

    updated information for hubspot

---
+++ projects/hubspot_with_testing/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  componentDidMount() {
+
+    const hubKey = process.env.REACT_APP_HUBKEY;
+  };
+
+  render() {
+    return (
+      <div>TESTIING PAGE </div>
+      )
+  };
+
+};
+    
+export default App;
